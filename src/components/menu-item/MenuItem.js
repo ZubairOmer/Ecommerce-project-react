df474477a0b9319da8120fe7520b022804f0c342
Zubair Omer <omer.zubair01@gmail.com>
2021-06-12 12:22:46 -0700

    minor bug fixed

--- src/components/menu-item/MenuItem.js
+++ src/components/menu-item/MenuItem.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import './MenuItem.css'
 
-const MenuItem = ({ title, imageUrl }) => {
+const MenuItem = ({ title, imageUrl, size }) => {
     return (
         <div style={{
             backgroundImage: `url(${imageUrl})`
-        }} className="menu-item">
+        }} className={`${size} menu-item`}
+        >
             <div className="content">
                 <h1 className="title">{title}</h1>
                 <span className="subtitle">SHOP NOW</span>
