52cc0f2cbf06ac7c61eca80b947d867715ff55b0
Zubair Omer <omer.zubair01@gmail.com>
2021-06-13 13:23:52 -0700

    header components styled well

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import './App.css'
 import HomePage from './pages/homepage/HomePage'
 import { Switch, Route } from 'react-router-dom'
 import Shop from './pages/shop/Shop'
-import './App.css'
+import Header from './components/header/Header'
 
 function App() {
     return (
         <div className="app">
+            <Header />
             <Switch>
                 <Route exact path='/' component={HomePage} />
                 <Route exact path='/shop' component={Shop} />
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import './Header.css'
+import { Link } from 'react-router-dom'
+import { ReactComponent as Logo } from '../../assets/4.3 crown.svg.svg'
 
 const Header = () => {
     return (
-        <div>
-
+        <div className='header'>
+            <Link to='/' className='logo-container'>
+                <Logo />
+            </Link>
+            <div className="options">
+                <Link to='/shop' className='option'>
+                    SHOP
+                </Link>
+                <Link to='/shop' className='option'>
+                    CONTACT
+                </Link>
+            </div>
         </div>
     )
 }
