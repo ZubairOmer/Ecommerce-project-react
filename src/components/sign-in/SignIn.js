cec0aa943b225db3f45bdb5ca5d37362760b65b4
Zubair Omer <omer.zubair01@gmail.com>
2021-06-17 22:58:56 -0700

    firestore items working

--- src/components/sign-in/SignIn.js
+++ src/components/sign-in/SignIn.js
@@ -50,9 +50,10 @@ export default class SignIn extends Component {
                         label='password'
                         required
                     />
-
-                    <CustomButton type="submit">Sign In</CustomButton>
-                    <CustomButton onClick={signInWithGoogle} isGoogleSignIn>Sign In with Google</CustomButton>
+                    <div className="buttons">
+                        <CustomButton type="submit">Sign In</CustomButton>
+                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>Sign In with Google</CustomButton>
+                    </div>
                 </form>
             </div>
         )
